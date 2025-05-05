287bf17590a79086b92dc2d692f10dd2f931c909
Rashid <arrc3174@gmail.com>
2025-05-05 13:37:19 -0600

    Actualizacion de archivo de la carpeta server para mostrar el frontend

--- server/index.js
+++ server/index.js
@@ -1,17 +1,32 @@
 const cors = require('cors');
 const express = require('express');
+const path = require('path');  // Necesario para servir archivos estáticos
 const routes = require('../routes');
-const path = require('path');
-
+const db = require('../database/models');
 
 const server = express();
 
-
+// 1. Configuración CORS
 server.use(cors());
 server.options('*', cors());
+
+// 2. Middlewares
 server.use(express.json());
 
+// 3. Servir el frontend estático PRIMERO
+server.use(express.static(path.join(__dirname, '../../public')));
+
+// 4. Conexión a la base de datos
+db.sequelize.sync()
+  .then(() => console.log('✔ Base de datos conectada'))
+  .catch(err => console.error('✖ Error de conexión a DB:', err));
+
+// 5. Rutas API (se montan después del frontend)
 server.use('/api', routes);
 
+// 6. Catch-all para SPA (opcional)
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
 
 module.exports = server;
